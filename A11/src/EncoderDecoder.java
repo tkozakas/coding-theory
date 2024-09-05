0d9eb859d337d6362ad569005d10a7db9466e0a1
tomas6446 <tomas.kozakas@gmail.com>
2024-09-05 14:45:51 +0300

    Corrections

--- A11/src/EncoderDecoder.java
+++ A11/src/EncoderDecoder.java
@@ -1,17 +1,17 @@
+import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class EncoderDecoder {
     private final Random random = new Random();
 
-    public int[] encode(int[] message, int[][] G) {
-        int n = G[0].length;
-        int[] encoded = new int[n];
-        for (int i = 0; i < message.length; i++) {
-            for (int j = 0; j < n; j++) {
-                encoded[j] = (encoded[j] + message[i] * G[i][j]) % 2;
-            }
-        }
-        return encoded;
+    public int[] encode(int[] u, int[][] G) {
+        return IntStream.range(0, G[0].length)
+                .map(j -> IntStream.range(0, G.length)
+                        .map(i -> G[i][j] * u[i])
+                        .sum() % 2)
+                .toArray();
     }
 
     public int[] sendThroughChannel(int[] message, double pe) {
@@ -26,15 +26,64 @@ public class EncoderDecoder {
         return transmitted;
     }
 
-    public int[] decode(int[] received) {
+    public int[] decode(int[] received, int[][] H) {
+        int[] syndrome = calculateSyndrome(received, H);
+        while (isNonZero(syndrome)) {
+            int errorPosition = findErrorPosition(syndrome, H);
+            if (errorPosition != -1) {
+                received[errorPosition] = 1 - received[errorPosition];
+            }
+            syndrome = calculateSyndrome(received, H);
+        }
         return received;
+    }
+
+    private int[] calculateSyndrome(int[] received, int[][] H) {
+        return Arrays.stream(H).mapToInt(ints -> IntStream.range(0, received.length)
+                        .map(j -> received[j] * ints[j])
+                        .sum() % 2)
+                .toArray();
+    }
+
+    private boolean isNonZero(int[] syndrome) {
+        return IntStream.of(syndrome).anyMatch(value -> value != 0);
+    }
+
+    private int findErrorPosition(int[] syndrome, int[][] H) {
+        for (int j = 0; j < H[0].length; j++) {
+            int[] column = getColumn(H, j);
+            if (Arrays.equals(syndrome, column)) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    private int[] getColumn(int[][] matrix, int column) {
+        return Arrays.stream(matrix).mapToInt(ints -> ints[column])
+                .toArray();
     }
 
     public String arrayToString(int[] array) {
         StringBuilder sb = new StringBuilder();
-        for (int value : array) {
-            sb.append(value);
+        sb.append("[");
+        for (int i = 0; i < array.length; i++) {
+            sb.append(array[i]);
+            if (i < array.length - 1) {
+                sb.append(", ");
+            }
         }
+        sb.append("]");
         return sb.toString();
+    }
+
+    public String matrixToString(int[][] g) {
+        return Arrays.stream(g)
+                .map(row -> arrayToString(row) + "\n")
+                .collect(Collectors.joining());
+    }
+
+    public int[][] generateParityMatrix(int[][] g) {
+        return new int[g[0].length - g.length][g[0].length];
     }
 }
--- A11/src/UserInterface.java
+++ A11/src/UserInterface.java
@@ -16,28 +16,34 @@ public class UserInterface {
         System.out.print("Įveskite kodo dimensiją (k): ");
         int k = scanner.nextInt();
 
-        int[][] g = new int[k][n];
-        System.out.println("Įveskite generuojančią matricą G (" + k + "x" + n + "):");
-        for (int i = 0; i < k; i++) {
-            for (int j = 0; j < n; j++) {
-                g[i][j] = scanner.nextInt();
-            }
-        }
+        int[][] G = generateMatrix(n, k);
+        System.out.println("Kodo matrica: \n" + encoderDecoder.matrixToString(G));
+        int[][] H = encoderDecoder.generateParityMatrix(G);
 
         System.out.print("Įveskite klaidos tikimybę (0 <= pe <= 1): ");
         double pe = scanner.nextDouble();
         System.out.print("Įveskite pranešimą kaip vektorių (pvz., 101): ");
         String input = scanner.next();
 
-        int[] message = IntStream.range(0, input.length()).map(i -> Character.getNumericValue(input.charAt(i))).toArray();
+        int[] u = IntStream.range(0, input.length()).map(i -> Character.getNumericValue(input.charAt(i))).toArray();
 
-        int[] encodedMessage = encoderDecoder.encode(message, g);
+        int[] encodedMessage = encoderDecoder.encode(u, G);
         System.out.println("Užkoduotas vektorius: " + encoderDecoder.arrayToString(encodedMessage));
 
         int[] transmittedMessage = encoderDecoder.sendThroughChannel(encodedMessage, pe);
         System.out.println("Kanalu išsiųstas vektorius: " + encoderDecoder.arrayToString(transmittedMessage));
 
-        int[] decodedMessage = encoderDecoder.decode(transmittedMessage);
+        int[] decodedMessage = encoderDecoder.decode(transmittedMessage, H);
         System.out.println("Dekoduotas vektorius: " + encoderDecoder.arrayToString(decodedMessage));
+    }
+
+    private int[][] generateMatrix(int n, int k) {
+        int[][] G = new int[k][n];
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < n; j++) {
+                G[i][j] = (j == i) ? 1 : 0;
+            }
+        }
+        return G;
     }
 }
